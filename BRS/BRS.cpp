23f22bc0295956fe40de7a2d491ab8b64147ceb7
jimi-f <jimifolarin@gmail.com>
2019-12-12 22:31:36 -0800

    Created cpp and h files

---
+++ BRS/BRS.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <string>
+#include "BRS.h"
+#include <iomanip> 
+using namespace std;
---
+++ BRS/BRS.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+class BRS
+{
+public:
+	BRS(void);
+
+
+private:
+
+};
+
+BRS::BRS()
+{
+}
